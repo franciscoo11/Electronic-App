e0cf223a979e3b11554f48fbc04f677b9c1da028
Francisco Giudicatti <78971729+franciscoo11@users.noreply.github.com>
2022-12-13 20:30:28 -0300

    fix some problems with express

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,6 @@ app.use(express.json());
 
 app.use('/api/v1/users', userRoutes);
 
-app.use((_req, res, next) => {
-  const error = new Error('not found');
-  return res.status(404).json({
-    message: error.message
-  });
-});
-
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
