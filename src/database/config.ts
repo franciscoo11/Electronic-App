3c71fab7a8e96deb40f6b46ad49086330b78b861
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-12-13 15:12:24 -0300

    change connection string DB

--- src/database/config.ts
+++ src/database/config.ts
@@ -5,15 +5,20 @@ const DB_URI = <string>process.env.MONGO_URI;
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-} as ConnectOptions);
-
-const connection = mongoose.connection;
-
-connection.once('open', () => {
+} as ConnectOptions).then(() => {
   console.log('DB connected');
-});
-
-connection.on('error', err => {
+}).catch(err => {
   console.error(err);
   process.exit(0);
 });
+
+// const connection = mongoose.connection;
+
+// connection.once('open', () => {
+//   console.log('DB connected');
+// });
+
+// connection.on('error', err => {
+//   console.error(err);
+//   process.exit(0);
+// });
