c077fa627859ad55382d0dcad806d4cdaf841df7
Ivan Garcia <hectorivang2000@gmail.com>
2022-12-14 12:53:43 -0500

    addproducts

---
+++ src/models/Products.ts
@@ -0,0 +1,37 @@
+import mongoose, { model, Schema } from 'mongoose';
+
+const productsSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: Array,
+    required: true,
+  },
+  reviews: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
+  ]
+  ,
+  inStock: {
+    type: Number,
+    required: true
+  }
+});
+
+// productsSchema.set('toJSON', {
+//   transform: (_document, returnedObject) => {
+//     returnedObject.id = returnedObject._id;
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   }
+// });
+
+export default model('Products', productsSchema);
