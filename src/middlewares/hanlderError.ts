0256d227f0ce41db5b697b397c8a8bcad2b78c2e
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-12-14 23:47:44 -0300

    fix bad folder for errors handlers

--- src/middlewares/hanlderError.ts
+++ src/middlewares/hanlderError.ts
@@ -1,14 +1,13 @@
 import { Response } from 'express';
 
-const handlerHttpError = (res: Response, error: Error) => {
+export const handlerHttpError = (res: Response, error: Error) => {
   console.log('Error server', error);
   res.status(500);
   res.send({ error: 'ERROR' });
 };
   
-const handlerRespError = (res: Response, message: string, code: number) => {
+export const handlerRespError = (res: Response, message: string, code: number) => {
   console.log('Error request', message);
   res.status(code);
   res.send({ error: message });
 };
-  
