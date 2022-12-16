536f49ed3f8701d2c0a4135944a97b91b75bcff2
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-12-15 22:58:56 -0300

    fixing handler error with server and better logic in usercontroller and authcontroller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,37 +1,48 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import User, { IUser } from '../models/User';
 import jwt from 'jsonwebtoken';
+import { handlerHttpError, handlerRespError } from '../helpers/handlerError';
 
 export const register = async (req: Request, res: Response) => {
-  const user: IUser = new User({
-    email: req.body.email,
-    name: req.body.name,
-    lastName: req.body.lastName,
-    password: req.body.password,
-    avatar: req.body.avatar
-  });
-  user.password = await user.encryptPassword(user.password);
+  try {
+    const findUserExist = await User.findOne({ email: req.body.email });
+    if (findUserExist) return handlerRespError(res, 'USER_ALREADY_EXIST', 401);
+  
+    const user: IUser = new User({
+      email: req.body.email,
+      name: req.body.name,
+      lastName: req.body.lastName,
+      password: req.body.password,
+      avatar: req.body.avatar
+    });
+    user.password = await user.encryptPassword(user.password);
 
-  const addUser = await user.save();
-
-  const token: string = jwt.sign({_id: addUser._id, role: addUser.role}, <string>process.env.JWT_SECRET,  { 
-    expiresIn: 60 * 60 * 24
-  });
-
-  return res.header('auth-token', token).json(addUser);
+    const addUser = await user.save();
+    return res.status(201).json(addUser);
+  } catch (error) {
+    handlerHttpError(res,error);
+  }
+  
 };
 
-export const login = async (req: Request, res: Response) => {
-  const user = await User.findOne({ email: req.body.email });
-  if(!user) return res.status(400).json('Wrong credentials');
-  const checkPassword = await user.comparePassword(req.body.password);
-  if(!checkPassword) return res.status(400).json('Invalid password');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const login = async (req: Request, res: Response, _next: NextFunction) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) return handlerRespError(res, 'USER_NOT_FOUND', 404);
 
-  const token: string = jwt.sign({_id: user._id, role: user.role}, <string>process.env.JWT_SECRET, { 
-    expiresIn: 60 * 60 * 24
-  });
-
-  return res.header('auth-token', token).json(user);
+    const checkPassword = await user.comparePassword(req.body.password);
+    if(!checkPassword) return handlerRespError(res, 'INVALID_PASSWORD', 404);
+  
+    const token: string = jwt.sign({_id: user._id, role: user.role}, <string>process.env.JWT_SECRET, { 
+      expiresIn: 60 * 60 * 24
+    });
+  
+    return res.status(200).json({ user, token });
+  } catch (error) {
+    handlerHttpError(res,error);
+  }
+ 
 };
 
 export const profile = async (req: Request, res: Response) => {
--- src/helpers/handlerError.ts
+++ src/helpers/handlerError.ts
@@ -1,13 +1,13 @@
 import { Response } from 'express';
 
 export const handlerHttpError = (res: Response, error: Error | unknown) => {
-  console.log('Error server', error);
+  console.log('Error server origin:', error);
   res.status(500);
-  res.send({ error: 'Something goes wrong, try again later' });
+  res.json({ error: 'Something goes wrong, try again later' });
 };
   
 export const handlerRespError = (res: Response, message: string, code: number) => {
   console.log('Error request', message);
   res.status(code);
-  res.send({ error: message });
+  res.json({ error: message });
 };
