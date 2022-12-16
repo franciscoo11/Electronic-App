7589bef052b4dc436f1b869b2a66b5cde92f46cd
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-12-16 17:02:30 -0300

    install autopopulate between relations documents and aggregatte route to global router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import authRouter from './authRoutes';
 import categoryRouter from './categoryRoutes';
 import userRouter from './userRoutes';
+import reviewRouter from './reviewRoutes';
+
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/categories', categoryRouter);
+router.use('/reviews', reviewRouter);
 
 export default router;
