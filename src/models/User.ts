02a95d2d6056a37c8125f6e9ab6f8cedc51cd7cf
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-12-19 13:19:41 -0300

    add process before save user encrypt password

--- src/models/User.ts
+++ src/models/User.ts
@@ -68,6 +68,17 @@ userSchema.methods.comparePassword = async function (password:string): Promise<b
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.pre('save', async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  const hash = await bcrypt.hash(user.password, 10);
+  user.password = hash;
+  next();
+});
+
 // userSchema.set('toJSON', {
 //   transform: (_document, returnedObject) => {
 //     returnedObject.id = returnedObject._id;
